63b3f96841d0aa86112817443fa6da349f6ebfd8
Wang Cong <ousatoru@hotmail.com>
2016-09-03 10:41:14 +0800

    开户申请表

--- CorporateCounter/Form1.Designer.cs
+++ CorporateCounter/Form1.Designer.cs
@@ -68,6 +68,7 @@
             this.compNametextBox1.Name = "compNametextBox1";
             this.compNametextBox1.Size = new System.Drawing.Size(287, 21);
             this.compNametextBox1.TabIndex = 0;
+            this.compNametextBox1.Text = "枯枯城 是 是是国";
             // 
             // licencetextBox2
             // 
@@ -75,6 +76,8 @@
             this.licencetextBox2.Name = "licencetextBox2";
             this.licencetextBox2.Size = new System.Drawing.Size(287, 21);
             this.licencetextBox2.TabIndex = 1;
+            this.licencetextBox2.Text = "9128343849392338S";
+            this.licencetextBox2.TextChanged += new System.EventHandler(this.licencetextBox2_TextChanged);
             // 
             // label1
             // 
@@ -104,7 +107,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "申请表";
+            this.button1.Text = "开户申请表";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -165,6 +168,7 @@
             this.scopetextBox2.Name = "scopetextBox2";
             this.scopetextBox2.Size = new System.Drawing.Size(287, 21);
             this.scopetextBox2.TabIndex = 7;
+            this.scopetextBox2.Text = "卖比比比比比比比";
             // 
             // label4
             // 
@@ -181,6 +185,7 @@
             this.addrtextBox1.Name = "addrtextBox1";
             this.addrtextBox1.Size = new System.Drawing.Size(287, 21);
             this.addrtextBox1.TabIndex = 5;
+            this.addrtextBox1.Text = "为这产是同上上上是中";
             // 
             // label3
             // 
@@ -220,6 +225,7 @@
             this.ownerPhonetextBox5.Name = "ownerPhonetextBox5";
             this.ownerPhonetextBox5.Size = new System.Drawing.Size(100, 21);
             this.ownerPhonetextBox5.TabIndex = 4;
+            this.ownerPhonetextBox5.Text = "4823029388";
             // 
             // ownerIDtextBox4
             // 
@@ -227,6 +233,7 @@
             this.ownerIDtextBox4.Name = "ownerIDtextBox4";
             this.ownerIDtextBox4.Size = new System.Drawing.Size(119, 21);
             this.ownerIDtextBox4.TabIndex = 3;
+            this.ownerIDtextBox4.Text = "838383847389220";
             // 
             // label6
             // 
@@ -243,6 +250,7 @@
             this.ownertextBox3.Name = "ownertextBox3";
             this.ownertextBox3.Size = new System.Drawing.Size(90, 21);
             this.ownertextBox3.TabIndex = 1;
+            this.ownertextBox3.Text = "2中4国";
             // 
             // label5
             // 
@@ -256,6 +264,8 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
             this.checkBox1.Location = new System.Drawing.Point(405, 12);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(72, 16);
@@ -346,6 +356,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "对公表格";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Company.ResumeLayout(false);
             this.Company.PerformLayout();
             this.panel1.ResumeLayout(false);
--- CorporateCounter/Form1.cs
+++ CorporateCounter/Form1.cs
@@ -63,6 +63,7 @@ namespace CorporateCounter
 
         private void requisition_PrintPage(object sender, PrintPageEventArgs e)
         {
+            DateTime dt = DateTime.Now;
             Font ft = new Font("宋体", 11, FontStyle.Bold);
             SolidBrush sb = new SolidBrush(Color.Black);
             PointF pfCompName1 = new PointF(150.0F, 150.0F);
@@ -83,13 +84,28 @@ namespace CorporateCounter
             string BankCode = GetAppConfig("BankCode");  //314311711013
             //账户性质 switch
             //行业分类 switch
-            e.Graphics.DrawString(CompName, ft, sb, new PointF(280.0F, 170.0F));
-            e.Graphics.DrawString(OwnerPhone, ft, sb, new PointF(700.0F, 170.0F));
-            e.Graphics.DrawString(Addr, ft, sb, new PointF(280.0F, 210.0F));
-            e.Graphics.DrawString(Postal, ft, sb, new PointF(700.0F, 210.0F));
-            e.Graphics.DrawString(OwnerName, ft, sb, new PointF(400.0F, 250.0F));
-            e.Graphics.DrawString(OwnerCer, ft, sb, new PointF(400.0F, 290.0F));
-            e.Graphics.DrawString(OwnerID, ft, sb, new PointF(700.0F, 290.0F));
+            e.Graphics.DrawString(CompName, ft, sb, new PointF(230.0F, 140.0F));
+            e.Graphics.DrawString(OwnerPhone, ft, sb, new PointF(600.0F, 140.0F));
+            e.Graphics.DrawString(Addr, ft, sb, new PointF(230.0F, 160.0F));
+            e.Graphics.DrawString(Postal, ft, sb, new PointF(600.0F, 160.0F));
+            e.Graphics.DrawString(OwnerName, ft, sb, new PointF(380.0F, 219.0F));
+            e.Graphics.DrawString(OwnerCer, ft, sb, new PointF(380.0F, 242.0F));
+            e.Graphics.DrawString(OwnerID, ft, sb, new PointF(585.0F, 242.0F));
+            e.Graphics.DrawString(AreaCode, ft, sb, new PointF(530.0F, 308.5F));
+            e.Graphics.DrawString(Scope, ft, sb, new PointF(230.0F, 330.0F));
+            e.Graphics.DrawString(CompCer, ft, sb, new PointF(230.0F, 354.0F));
+            e.Graphics.DrawString(Licence, ft, sb, new PointF(527.0F, 354.0F));
+            e.Graphics.DrawString(Tax, ft, sb, new PointF(380.0F, 407.0F));
+            e.Graphics.DrawString(BankName, ft, sb, new PointF(225.0F, 700.0F));
+            e.Graphics.DrawString(BankCode, ft, sb, new PointF(530.0F, 700.0F));
+            e.Graphics.DrawString(CompName, new Font("宋体", 9.5F, FontStyle.Bold), 
+                sb, new PointF(225.0F, 725.0F));
+            e.Graphics.DrawString(dt.Year.ToString(), ft, sb, new PointF(180.0F, 895.0F));
+            e.Graphics.DrawString(dt.Month.ToString(), ft, sb, new PointF(228.0F, 895.0F));
+            e.Graphics.DrawString(dt.Day.ToString(), ft, sb, new PointF(262.0F, 895.0F));
+            e.Graphics.DrawString(dt.Year.ToString(), ft, sb, new PointF(368.0F, 895.0F));
+            e.Graphics.DrawString(dt.Month.ToString(), ft, sb, new PointF(415.0F, 895.0F));
+            e.Graphics.DrawString(dt.Day.ToString(), ft, sb, new PointF(448.0F, 895.0F));
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -106,7 +122,7 @@ namespace CorporateCounter
         {
             if(checkBox1.CheckState == CheckState.Checked)
             {
-
+                taxtextBox7.Text = licencetextBox2.Text;
             }
             else if(checkBox1.CheckState == CheckState.Unchecked)
             {
@@ -125,6 +141,19 @@ namespace CorporateCounter
             cm.AppSettings.Settings.Add("AreaCode", "311700");
             cm.Save(ConfigurationSaveMode.Modified);
             ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        private void licencetextBox2_TextChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.CheckState == CheckState.Checked)
+            {
+                taxtextBox7.Text = licencetextBox2.Text;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
